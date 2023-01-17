b7fee3cd02854cd6bbaad72fb9661dafe08468fd
Kiven Alaric <elmozaro3@gmail.com>
2023-01-17 13:31:14 -0600

    adjustment of things on pages and responsiveness

--- src/Pages/About/AbtHero/AbtHero.jsx
+++ src/Pages/About/AbtHero/AbtHero.jsx
@@ -9,27 +9,35 @@ export default function HeroSection() {
       <div className={AbtHeroCss.abthero__left}>
         <h1>We are dedicated to providing the best travel experience.</h1>
         <p>Discover the Land of a Thousand Hills</p>
-        <div className={AbtHeroCss.abthero__dropdown}>
-          <label htmlFor="guest" id="guest">
-            Guest
-          </label>
-          <select>
-            <option htmlFor="guest">2 adults</option>
-          </select>
-          <label htmlFor="date" id="date">
-            Guest
-          </label>
-          <select>
-            <option htmlFor>2 adults</option>
-          </select>
-          <label htmlFor="pack" id="pack">
-            Guest
-          </label>
-          <select>
-            <option htmlFor>2 adults</option>
-          </select>
-          <div className={AbtHeroCss.abthero__empty} />
-          <Button text="Book Now" color="#c7923c" />
+        <div className={AbtHeroCss.abt__booking}>
+          <div className={AbtHeroCss.abthero__dropdown}>
+            <div className={AbtHeroCss.abt__select}>
+              <label htmlFor="guest" id="guest">
+                Guest
+              </label>
+              <select>
+                <option htmlFor="guest">2 adults</option>
+              </select>
+            </div>
+            <div className={AbtHeroCss.abt__select}>
+              <label htmlFor="date" id="date">
+                Date
+              </label>
+              <select>
+                <option htmlFor>12 - 13 April 2021</option>
+              </select>
+            </div>
+            <div className={AbtHeroCss.abt__select}>
+              <label htmlFor="pack" id="pack">
+                Package
+              </label>
+              <select>
+                <option htmlFor>Akagera Tour</option>
+              </select>
+            </div>
+            <div className={AbtHeroCss.abthero__btn} />
+            <Button text="Book Now" color="#c7923c" />
+          </div>
         </div>
       </div>
       <div className={AbtHeroCss.abthero__right} />
